eba9f4d3d864e449fd91187167eac1dbe75ae7ee
clement <clemvit8@gmail.com>
2023-10-16 14:53:49 +0200

    ajout de la div de la lecture en cours

---
+++ my-ts-app/src/components/musicPlayed.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+
+export function MusicPlayed() {
+    return (
+        <footer>
+            <div style={{backgroundColor: "grey", position: "absolute", bottom: "0", width: "1848px", paddingBottom: "5px"}}>
+                <nav className="mx-auto flex max-w-8xl items-center justify-between lg:px-8" aria-label="Global">
+                    <div>
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <img className="items-left h-12 w-auto" src="../../asset/spotify.png" />
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#" className="font-semibold leading-6 text-gray-900 text-1xl">
+                            Title
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#" className="font-semibold leading-6 text-gray-900 text-1xl">
+                            Authors
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#" className="font-semibold leading-6 text-gray-900 text-1xl">
+                            Duration
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#" className="font-semibold leading-6 text-gray-900 text-1xl">
+                            start
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </footer>
+    )
+}
+        
